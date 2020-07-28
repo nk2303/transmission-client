5830b282ec32334a6ef30c5da223c1f9e6e21271
nk2303 <ngankhong03@gmail.com>
2020-07-28 16:05:38 -0700

    set up api

---
+++ src/services/api.js
@@ -0,0 +1,50 @@
+const BACKEND_DOMAIN = process.env.TRANSMISSION_APP_BACKEND_DOMAIN;
+// const key = () => localStorage.getItem("key");
+
+const headers = () => {
+  return {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    // Authorization: token()
+  };
+};
+
+const createPage = (key, content) => {
+    const page = { page: { key, content } }
+  return fetch(`${BACKEND_DOMAIN}/pages`, {
+    method: "POST",
+    headers: headers(),
+    body: JSON.stringify(page)
+  }).then(resp => resp.json());
+};
+
+// const getPage = () => {
+//   return fetch(`${BACKEND_DOMAIN}/current_user`, {
+//     headers: headers()
+//   }).then(resp => resp.json());
+// };
+
+const updatePage = data => {
+  return fetch(`${BACKEND_DOMAIN}/users/${data.user.id}`, {
+    method: "PUT",
+    headers: headers(),
+    body: JSON.stringify(data)
+  }).then(resp => resp.json());
+};
+
+const deletePage = page_id => {
+  return fetch(`${BACKEND_DOMAIN}/pages/${page_id}`, {
+    method: "DELETE",
+    headers: headers(),
+  }).then(resp => resp.json());
+};
+
+
+export const api = {
+  page: {
+    createPage,
+    // getPage,
+    updatePage,
+    deletePage,
+  }
+};
