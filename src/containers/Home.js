a6ba8a2a1bc30e49ee323ff9dfab2bebab1e6c52
nk2303 <ngankhong03@gmail.com>
2020-07-28 17:54:25 -0700

    generate ransome key

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,11 +4,19 @@ import Button from "react-bootstrap/Button";
 const Home = () => {
 
     const generateKey = () => {
-        const character = [ 
+        const characters = [ 
             "A",  "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+            "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
         ]
+        let r = '';
+        for (let i = 0; i <= 3; i++) {
+            let k = characters[Math.floor(Math.random()*characters.length)]
+            r = r + k;
+        }
+        return r;
     }
 
     return (
